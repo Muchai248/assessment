108d921ead2d7c6a9eae5f237e26724313061e1c
Mark Muchai <muchaimark6@gmail.com>
2023-08-29 11:49:31 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# Stacks
+
+def is_balanced(expression):
+    stack = []
+    for char in expression:
+        if char in "{[(":
+            stack.append(char)
+        elif char in "}])":
+            if not stack:
+                return False
+            if char == "}" and stack[-1] == "{":
+                stack.pop()
+            elif char == "]" and stack[-1] == "[":
+                stack.pop()
+            elif char == ")" and stack[-1] == "(":
+                stack.pop()
+            else:
+                return False
+    return not stack
+
+
+def remove_duplicates(sequence):
+    unique_elements = set(sequence)
+    return list(unique_elements),
+
+def word_frequency(sentence):
+    words = sentence.lower().split()
+    word_frequency = {}
+    for word in words:
+        if word not in word_frequency:
+            word_frequency[word]
+            
+sentence = "This is a test sentence. This sentence is a test."
+result = word_frequency(sentence)
+print(result)
+
+expression1 = "([]{})"
+expression2 = "([)]"
+print(is_balanced(expression1)) 
+print(is_balanced(expression2))
